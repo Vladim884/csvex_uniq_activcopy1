90ad7baa6dfc80430ed1f0b3a6b7a5c92c20885e
vladimir <vladim.volvenko@gmail.com>
2022-10-12 13:31:28 +0300

    creaned classes for auth/payment controllers and fix bag for these funcs

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -12,86 +12,202 @@ const {
     getUserfromToken,
     decryptToken} = require('../myFunctions/myFunctions')
 
-
-
-exports.writePaying = async (req, res) => {
-    const xtext = req.cookies.xtext
-    const token = decryptToken(xtext, config.get('secretKeyForToken1'))
-    if(!token){
-        return res.status(403).json({"message": "Ви не авторизувались"})
-    }
-    const datatoken = jwt.verify(token, config.get('secretKey'))
-    let userRole = datatoken.userRole
-    if(userRole !== 'admin') return res.render('msg', {msg: 'У Вас не має права доступу!'})
-    let {email, sumpay} = req.body
-    let user = await User.findOne({email})
-        if (!user) {
-            return res.status(404).json({message: "User not found"})
+class paymentController {
+    async writePaying (req, res) {
+        const xtext = req.cookies.xtext
+        const token = decryptToken(xtext, config.get('secretKeyForToken1'))
+        if(!token){
+            return res.status(403).json({"message": "Ви не авторизувались"})
         }
-    //==================
-    let number = ++user.paymentNumber
-    user.paymentNumber = number
-    user.payments.push({number, date: new Date, sum: sumpay})
-    //==================
-    let lastPayment = user.payments[user.payments.length - 1]
-    //=================================
-    let daysPaying = lastPayment.sum / (100/30)
-    // console.log(daysPaying)
-    //=====================
-    // console.log(lastPayment.date)
-    // console.log(user.endDay)
-    let datesDifferent = getNumberOfDays(lastPayment.date, user.endDay)
-    // console.log(`datesDifferent: ${datesDifferent}`)
-    // let lastPaymentDate = lastPayment.date
-    let sumdays = datesDifferent + daysPaying
-    let D = new Date(lastPayment.date)
-    
-    //paymentDateEnd:
-    //datsLeftActiveServise
-    if(datesDifferent > 0) {
-        user.endDay = D.setDate(D.getDate() + sumdays)
-        user.daysLeft = datesDifferent + daysPaying
-        clg(`3 lastPayment.date: ${lastPayment.date}`)
-    } else {
-        user.endDay = D.setDate(D.getDate() + daysPaying)
-        user.daysLeft = daysPaying
-    }
-    // console.log(`user.endDay: ${user.endDay}`)
-    // console.log(`user.daysLeft: ${user.daysLeft}`)
-
-    //========================
-
-    user.sumpay = +user.sumpay + +lastPayment.sum
-    // console.log(`user.sumpay: ${user.sumpay}`)
-    //===========================
-    user.balance = user.daysLeft * 100 / 30
-    // console.log(`user.balance: ${user.balance}`)
+        const datatoken = jwt.verify(token, config.get('secretKey'))
+        let userRole = datatoken.userRole
+        if(userRole !== 'admin') return res.render('msg', {msg: 'У Вас не має права доступу!'})
+        let {email, sumpay} = req.body
+        let user = await User.findOne({email})
+            if (!user) {
+                return res.status(404).json({message: "User not found"})
+            }
+        //==================
+        let number = ++user.paymentNumber
+        user.paymentNumber = number
+        user.payments.push({number, date: new Date, sum: sumpay})
+        //==================
+        let lastPayment = user.payments[user.payments.length - 1]
+        //=================================
+        let daysPaying = lastPayment.sum / (100/30)
+        // console.log(daysPaying)
+        //=====================
+        // console.log(lastPayment.date)
+        // console.log(user.endDay)
+        let datesDifferent = getNumberOfDays(lastPayment.date, user.endDay)
+        // console.log(`datesDifferent: ${datesDifferent}`)
+        // let lastPaymentDate = lastPayment.date
+        let sumdays = datesDifferent + daysPaying
+        let D = new Date(lastPayment.date)
         
-    let obj1 = {
-        payingDate: lastPayment.date,
-        daysPaying,
-    }
-    user = _.extend(user, obj1)
-    user.save((err, result) => {
-        if(err){
-            return res.status(400).render('msg', {msg: `Ошибка изменения оплати юзера ${email}`})
+        //paymentDateEnd:
+        //datsLeftActiveServise
+        if(datesDifferent > 0) {
+            user.endDay = D.setDate(D.getDate() + sumdays)
+            user.daysLeft = datesDifferent + daysPaying
+            clg(`3 lastPayment.date: ${lastPayment.date}`)
         } else {
-            console.log(`7 lastPayment.date: ${lastPayment.date}`)
-            let payingDateForPeople = formatNowDate(lastPayment.date)
-            emailOptionsSend(
-                'ivladim95@gmail.com',
-                'Оплата на CSV TO EXCEL.',
-                `${user.nicname}, Вас вітає команда CSV TO EXCEL!
-                Дякуємо, що Ви обрали наш сервіс!
-                 ${payingDateForPeople} Ви оплатили ${sumpay}грн. та отримали активацію сервісу CSV TO EXCEL 
-                 на ${daysPaying} днів.
-                 ===============================================
-                 Якщо цей лист потрапив до вас випадково, 
-                 видалить його та не звертайте уваги.
-                `
-            )
-            // return res.status(200).json({message: `Оплату юзера ${email} змінено`})
-            return res.status(200).render('msg', {msg: `Оплату юзера ${email} успішно змінено`})
+            user.endDay = D.setDate(D.getDate() + daysPaying)
+            user.daysLeft = daysPaying
         }
-    })
+        // console.log(`user.endDay: ${user.endDay}`)
+        // console.log(`user.daysLeft: ${user.daysLeft}`)
+    
+        //========================
+    
+        user.sumpay = +user.sumpay + +lastPayment.sum
+        // console.log(`user.sumpay: ${user.sumpay}`)
+        //===========================
+        user.balance = user.daysLeft * 100 / 30
+        // console.log(`user.balance: ${user.balance}`)
+            
+        let obj1 = {
+            payingDate: lastPayment.date,
+            daysPaying,
+        }
+        user = _.extend(user, obj1)
+        user.save((err, result) => {
+            if(err){
+                return res.status(400).render('msg', {msg: `Ошибка изменения оплати юзера ${email}`})
+            } else {
+                console.log(`7 lastPayment.date: ${lastPayment.date}`)
+                let payingDateForPeople = formatNowDate(lastPayment.date)
+                emailOptionsSend(
+                    'ivladim95@gmail.com',
+                    'Оплата на CSV TO EXCEL.',
+                    `${user.nicname}, Вас вітає команда CSV TO EXCEL!
+                    Дякуємо, що Ви обрали наш сервіс!
+                     ${payingDateForPeople} Ви оплатили ${sumpay}грн. та отримали активацію сервісу CSV TO EXCEL 
+                     на ${daysPaying} днів.
+                     ===============================================
+                     Якщо цей лист потрапив до вас випадково, 
+                     видалить його та не звертайте уваги.
+                    `
+                )
+                // return res.status(200).json({message: `Оплату юзера ${email} змінено`})
+                return res.status(200).render('msg', {msg: `Оплату юзера ${email} успішно змінено`})
+            }
+        })
+    }
+
+    async sendEndPay (req, res) {
+        const users = await User.find()
+        for (let i = 0; i < users.length; i++) {
+            const restDay = Math.round((users[i].endDay - new Date()) / (60 * 60 * 24 * 1000))
+            clg('restDay', `${restDay}`)
+            if (restDay < 9 && restDay > 0){
+                    let nowday = formatNowDate()
+                    let endDay = formatDate(restDay)
+                    clg('endDay', `${endDay}`)
+                    emailOptionsSend(
+                        'ivladim95@gmail.com',
+                        'Оплата на CSV TO EXCEL.',
+                        `
+                         Доброго дня!
+                        Сьогодні ${nowday} у Вас залишилось ${restDay} днів до ${endDay} включно.
+                        Потурбуйтеся про своєчасну оплату сервісу!
+                         ===============================================
+                         Ваши 
+                         логин: ${users[i].email} 
+                         Якщо цей лист потрапив до вас випадково, 
+                         видалить його та не звертайте уваги.
+                        `
+                    )
+                    setTimeout(() => {}, 3000);
+            }
+            
+        }
+        alert('Завершено')
+        res.json({message: 'Листи о скором завершенні дії сервісу відправлено'})
+    }
 }
+
+module.exports = new paymentController()
+
+
+// exports.writePaying = async (req, res) => {
+//     const xtext = req.cookies.xtext
+//     const token = decryptToken(xtext, config.get('secretKeyForToken1'))
+//     if(!token){
+//         return res.status(403).json({"message": "Ви не авторизувались"})
+//     }
+//     const datatoken = jwt.verify(token, config.get('secretKey'))
+//     let userRole = datatoken.userRole
+//     if(userRole !== 'admin') return res.render('msg', {msg: 'У Вас не має права доступу!'})
+//     let {email, sumpay} = req.body
+//     let user = await User.findOne({email})
+//         if (!user) {
+//             return res.status(404).json({message: "User not found"})
+//         }
+//     //==================
+//     let number = ++user.paymentNumber
+//     user.paymentNumber = number
+//     user.payments.push({number, date: new Date, sum: sumpay})
+//     //==================
+//     let lastPayment = user.payments[user.payments.length - 1]
+//     //=================================
+//     let daysPaying = lastPayment.sum / (100/30)
+//     // console.log(daysPaying)
+//     //=====================
+//     // console.log(lastPayment.date)
+//     // console.log(user.endDay)
+//     let datesDifferent = getNumberOfDays(lastPayment.date, user.endDay)
+//     // console.log(`datesDifferent: ${datesDifferent}`)
+//     // let lastPaymentDate = lastPayment.date
+//     let sumdays = datesDifferent + daysPaying
+//     let D = new Date(lastPayment.date)
+    
+//     //paymentDateEnd:
+//     //datsLeftActiveServise
+//     if(datesDifferent > 0) {
+//         user.endDay = D.setDate(D.getDate() + sumdays)
+//         user.daysLeft = datesDifferent + daysPaying
+//         clg(`3 lastPayment.date: ${lastPayment.date}`)
+//     } else {
+//         user.endDay = D.setDate(D.getDate() + daysPaying)
+//         user.daysLeft = daysPaying
+//     }
+//     // console.log(`user.endDay: ${user.endDay}`)
+//     // console.log(`user.daysLeft: ${user.daysLeft}`)
+
+//     //========================
+
+//     user.sumpay = +user.sumpay + +lastPayment.sum
+//     // console.log(`user.sumpay: ${user.sumpay}`)
+//     //===========================
+//     user.balance = user.daysLeft * 100 / 30
+//     // console.log(`user.balance: ${user.balance}`)
+        
+//     let obj1 = {
+//         payingDate: lastPayment.date,
+//         daysPaying,
+//     }
+//     user = _.extend(user, obj1)
+//     user.save((err, result) => {
+//         if(err){
+//             return res.status(400).render('msg', {msg: `Ошибка изменения оплати юзера ${email}`})
+//         } else {
+//             console.log(`7 lastPayment.date: ${lastPayment.date}`)
+//             let payingDateForPeople = formatNowDate(lastPayment.date)
+//             emailOptionsSend(
+//                 'ivladim95@gmail.com',
+//                 'Оплата на CSV TO EXCEL.',
+//                 `${user.nicname}, Вас вітає команда CSV TO EXCEL!
+//                 Дякуємо, що Ви обрали наш сервіс!
+//                  ${payingDateForPeople} Ви оплатили ${sumpay}грн. та отримали активацію сервісу CSV TO EXCEL 
+//                  на ${daysPaying} днів.
+//                  ===============================================
+//                  Якщо цей лист потрапив до вас випадково, 
+//                  видалить його та не звертайте уваги.
+//                 `
+//             )
+//             // return res.status(200).json({message: `Оплату юзера ${email} змінено`})
+//             return res.status(200).render('msg', {msg: `Оплату юзера ${email} успішно змінено`})
+//         }
+//     })
+// }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,45 +5,39 @@ const jwt = require("jsonwebtoken")
 const {check, validationResult} = require("express-validator")
 const router = new Router()
 const {cookieJwtAuth} = require('../middleware/cookieJwtAuth')
-const {writePaying} = require('../controllers/paymentController')
-const { 
-        signup, 
-        activateAccount, 
-        forgotPassword, 
-        resetPassword,
-        sendEndPay,
-        getTokenUserData, 
-        getAccessToStart,
-        continueWork,
-        logout,
-        login} = require('../controllers/authController')
+const authController = require("../controllers/authController")
+const paymentController = require("../controllers/paymentController")
+// const { 
+//         signup, 
+//         activateAccount, 
+//         forgotPassword, 
+//         resetPassword,
+//         sendEndPay,
+//         getTokenUserData, 
+//         getAccessToStart,
+//         continueWork,
+//         logout,
+//         login} = require('../controllers/authController')
 
 router.post('/registration',
     [
         check('email', "Uncorrect email").isEmail(),
         check('password', 'Password must be longer than 3 and shorter than 12').isLength({min:3, max:12})
     ],
-    signup)
+    authController.signup)
    
-router.post('/email-activate', activateAccount)
-router.post('/forgot-password', forgotPassword)
-router.post('/resset-pass', resetPassword)
-router.post('/login', login)
-router.get('/logout', cookieJwtAuth, logout)
+router.post('/email-activate', authController.activateAccount)
+router.post('/forgot-password', authController.forgotPassword)
+router.post('/resset-pass', authController.resetPassword)
+router.post('/login', authController.login)
+router.get('/logout', cookieJwtAuth, authController.logout)
 router.get("/start", cookieJwtAuth, function(req, res){
     res.render('start.hbs')
 })
 router.get('/user', cookieJwtAuth, async function(req, res){
     const user = await User.findOne({email: 'vov2@gmail.com'})
     console.log(`users-users: ${user}`)
-    // res.end({user: `${user}`})
-    // res.end({user: {
-    //     id: user.id,
-    //     email: user.email,
-    //     diskSpace: user.diskSpace,
-    //     usedSpace: user.usedSpace,
-    //     avatar: user.avatar
-    // }});
+    
     res.json({user: {
             id: user.id,
             email: user.email,
@@ -54,9 +48,8 @@ router.get('/user', cookieJwtAuth, async function(req, res){
             
 });
 
-router.post('/writepaying', [cookieJwtAuth], writePaying)
-router.post('/sendendpay', cookieJwtAuth, sendEndPay)
-router.get('/usercabinet', cookieJwtAuth, getTokenUserData)
-router.get('/payhistory', cookieJwtAuth, getTokenUserData)
+router.post('/writepaying', [cookieJwtAuth], paymentController.writePaying)
+router.post('/sendendpay', cookieJwtAuth, paymentController.sendEndPay)
+router.get('/usercabinet', cookieJwtAuth, authController.getTokenUserData)
 
 module.exports = router
