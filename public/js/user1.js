24a9907de7da00b8da6e2b77dc8f4fe4449e8def
vladimir <vladim.volvenko@gmail.com>
2022-11-12 20:52:58 +0200

    intermediate step pagination

--- public/js/user1.js
+++ public/js/user1.js
@@ -1,55 +1,37 @@
 const pageTitle = document.querySelector("h1.users-page-title")
 const userList = document.querySelector("ul.users-list")
 const paginator = document.querySelector("ul.paginator")
-let flag = 0
 const arLi = paginator.querySelectorAll('li')
 console.log(arLi)
 
 let elems = []
-console.log(flag)
-if(flag === 0){
-    getUser()
-}
+getUser()
 async function getUser() {
-    
-        const response = await fetch("http://localhost:5000/api/payment/users1", { 
-            method: "get", 
-            headers: { "Content-Type": "application/json" }
-            
-        })
-        
-        // console.log(res)
-        // if (response.ok === true) {
-            const data = await response.json()
-        // }
-
+    const response = await fetch("http://localhost:5000/api/payment/users1", { 
+        method: "get", 
+        headers: { "Content-Type": "application/json" }
+    })
+    // if (response.ok === true) {
+    const data = await response.json()
     console.log(data)
+
     const usersList = data.paginationData.usersData
-    const pagesCount = data.paginationData.pagesCount
-    let currentPage = data.paginationData.currentPage
-    let rows = data.paginationData.rows
     let pages = data.paginationData.pages
-    // const nicname = data.userData.nicname
-    console.log(usersList)
-    // console.log(nicname)
+    // console.log(usersList)
     if(!usersList) {
         pageTitle.textContent = `Ви не авторизовані! Виповніть вхід`
     } else {
         pageTitle.textContent = `Перелік користувачів сайту`
     }
     
-    
+    //start display user-list
     displayUserList()
     function displayUserList(){
-        
         for (i=0;i<=usersList.length-1;i++) {
             //создаем элемент
             var elem = document.createElement("li");
             //создаем для него текст
-            var elemText = document.createTextNode(`
-                ${usersList[i].nicname}
-
-                    `)
+            var elemText = document.createTextNode(`${usersList[i].nicname}`)
             //добавляем текст в элемент в качестве дочернего элемента
             elem.appendChild(elemText);
             //добавляем элемент в блок div
@@ -57,10 +39,10 @@ async function getUser() {
         }
         function displayPaginationBtn(){
             // debugger
-            //создаем элемент
-            
-            for (i=0;i<pages;i++) {
+           for (i=0;i<pages;i++) {
                 console.log(pages)
+
+                //create element
                 const el = document.createElement("li")
                 var elText = document.createTextNode(`${i + 1}`)
                 
@@ -76,69 +58,45 @@ async function getUser() {
                 elems[i].addEventListener('click', displayUsersPage1)
             }
         }
-        
         displayPaginationBtn()
-        
     }
-
 }
 
 
-
-async function displayUsersPage1 () {
+//display user-list after click on paginator-btns
+async function displayUsersPage1 (rows) {
     // debugger
-    flag = 1
-    let currentPage = this.innerText
-    console.log(currentPage)
-        const response = await fetch("http://localhost:5000/api/payment/users1", {
-            method: "POST",
-            headers: { "Accept": "application/json", "Content-Type": "application/json" },
-            body: JSON.stringify({
-                currentPage: +currentPage - 1
-            })
-        });
-        if (response.ok === true) {
-            const data = await response.json()
-            console.log(data)
-            let usersList = data.paginationData.usersData
-            userList.innerHTML = ""
 
-            //=========
-            for (i=0;i<=usersList.length-1;i++) {
-                //создаем элемент
-                var elem = document.createElement("li");
-                //создаем для него текст
-                var elemText = document.createTextNode(`
-                    ${usersList[i].nicname}
-    
-                        `)
-                //добавляем текст в элемент в качестве дочернего элемента
-                elem.appendChild(elemText);
-                //добавляем элемент в блок div
-                userList.appendChild(elem)
-            }
-            // function displayPaginationBtn(){
-            //     // debugger
-            //     //создаем элемент
-                
-            //     for (i=0;i<pages;i++) {
-            //         console.log(pages)
-            //         const el = document.createElement("li")
-            //         var elText = document.createTextNode(`${i + 1}`)
-                    
-            //         //добавляем текст в элемент в качестве дочернего элемента
-            //         el.appendChild(elText);
-            //         //добавляем элемент в блок ul
-            //         paginator.appendChild(el)
-            //         elems.push(el)
-            //     }
-            //     console.log(Object.values(elems))
-            //     for(i=0; i< elems.length; i++){
-                
-            //         elems[i].addEventListener('click', displayUsersPage1)
-            //     }
-            // }
-            //============
+    //currentPage must be a number
+    let currentPage = +this.innerText-1 
+    // console.log(currentPage)
+    const response = await fetch("http://localhost:5000/api/payment/users1", {
+        method: "POST",
+        headers: { "Accept": "application/json", "Content-Type": "application/json" },
+        body: JSON.stringify({
+            currentPage,
+            rows
+        })
+    });
+    if (response.ok === true) {
+        const data = await response.json()
+        console.log(data)
+        let usersList = data.paginationData.users
+        userList.innerHTML = ""
+
+        for (i=0;i<=usersList.length-1;i++) {
+            //create element
+            var elem = document.createElement("li")
+
+            //create text for element
+            var elemText = document.createTextNode(` ${usersList[i].nicname}`)
+
+            //adding text into element
+            elem.appendChild(elemText)
+
+            //adding element into ul - .userList
+            userList.appendChild(elem)
         }
+    }
 }
 
--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -21,7 +21,7 @@ router.post('/users', cookieJwtAuth, async function(req, res){
 router.get('/users1', cookieJwtAuth, async function(req, res){
     try {
         let currentPage = 0
-        let rows = 5
+        let rows = 10
         const userData = await User.find()
         const users = []
         for (let i = 0; i < userData.length; i++) {
@@ -51,38 +51,41 @@ router.get('/users1', cookieJwtAuth, async function(req, res){
     }
             
 })
-router.post('/users1', cookieJwtAuth, async function(req, res){
+router.post('/users1', cookieJwtAuth, async function(req, res, next){
     try {
         if(!req.body) return res.sendStatus(400)
-        console.log('POST')
         let currentPage = req.body.currentPage
-        let rows = 5
-        const userData = await User.find()
+        let rows = 10
+        const allUsersData = await User.find()
+
+        const pages = Math.ceil(allUsersData.length / rows)
+
+        const start = rows * currentPage
+        const end = start + rows;
+        const usersData = allUsersData.slice(start, end)
+
         const users = []
-        for (let i = 0; i < userData.length; i++) {
+        for (let i = 0; i < usersData.length; i++) {
             let user = {
-                nicname: userData[i].nicname,
-                registrDate: userData[i].registrDate,
-                email: userData[i].email,
-                role: userData[i].status,
-                balance: userData[i].balance,
-                endDay: userData[i].endDay
+                nicname: usersData[i].nicname,
+                registrDate: usersData[i].registrDate,
+                email: usersData[i].email,
+                role: usersData[i].status,
+                balance: usersData[i].balance,
+                endDay: usersData[i].endDay
             }
             users.push(user)
         }
         // console.log(users)
 
-        const pages = Math.ceil(userData.length / rows)
+        
 
-        const start = rows * (currentPage)
-        const end = start + rows;
-        const usersData = users.slice(start, end)
-
-        const paginationData = {usersData, currentPage, rows, pages}
+        const paginationData = {users, currentPage, rows, pages}
         return res.json({paginationData})
         // return res.render('service/users', {nicname1: 'vladim1', pagenumber3: 3})
     } catch (error) {
             console.log(error)
+            next(error)
     }
             
 })
