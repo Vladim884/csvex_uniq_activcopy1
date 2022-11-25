3c522f88a19d7083227212a8d1da724d8c24e3d4
vladimir <vladim.volvenko@gmail.com>
2022-11-26 01:59:44 +0200

    the paginator works, but the code needs to be edited

--- public/js/usersList.js
+++ public/js/usersList.js
@@ -3,8 +3,7 @@ window.addEventListener('load', function(){
     const pageTitle = document.querySelector("h1.users-page-title")
     const userList = document.querySelector("ul.users-list")
     const paginator = document.querySelector("ul.paginator")
-    const arLi = paginator.querySelectorAll('li')
-    console.log(arLi)
+    
 
     let elems = []
     getUser()
@@ -26,7 +25,12 @@ window.addEventListener('load', function(){
             pageTitle.textContent = `Перелік користувачів сайту`
         }
         let pages = data.paginationData.pages
-        let portions = Math.ceil(pages / 4)
+        let currentPortion = data.paginationData.currentPortion
+        let portionSize = data.paginationData.portionSize
+        let start = data.paginationData.start
+        let portions = data.paginationData.portions
+
+        // let portions = Math.ceil(pages / 4)
         
         //start display users-list
         displayUserList()
@@ -56,22 +60,35 @@ window.addEventListener('load', function(){
                 paginator.appendChild(elPrev)
 
                 // debugger
-                for (i=0;i<pages;i++) {
-                    console.log(pages)
+                for (i=0;i<portionSize;i++) {
+                    console.log(portionSize)
 
                     //create element
                     const el = document.createElement("li")
-                    var elText = document.createTextNode(`${i + 1}`)
+                    // 
+                    var elText = document.createTextNode(`${++start}`)
+                    // console.log(`start: ${start}`)
                     
                     //добавляем текст в элемент в качестве дочернего элемента
                     el.appendChild(elText);
                     //добавляем элемент в блок ul
                     paginator.appendChild(el)
                     elems.push(el)
+                    console.log(`elems: ${elems}`)
+                    if(elems.length === pages) break
+                    // if(elems.length === pages) break
+                    // if(i === 0) el[i].classList.add('active')
+                    //
                 }
-                // console.log(Object.values(elems))
+
+                const arrPaginBtn = paginator.querySelectorAll('li')
+                arrPaginBtn[0].classList.add('active')
+                console.log(`arLi: ${Object.values(arrPaginBtn)}`)
+                // console.log(elems)
+                console.log(Object.values(elems))
                 for(i=0; i< elems.length; i++){
-                    elems[0].classList.add('active')
+                    // if(i === elems[i].innerText - 1) elems[i].classList.add('active')
+                    // elems[0].classList.add('active')
                     elems[i].addEventListener('click', displayUsersPage1)
                 }
 
@@ -80,6 +97,117 @@ window.addEventListener('load', function(){
                 var elNextText = document.createTextNode(`next`)
                 elNext.appendChild(elNextText)
                 paginator.appendChild(elNext)
+
+                //hidden next-button
+                if(currentPortion === portions) {
+                    elNext.classList.add('hidden')
+                    // return
+                }
+
+                if(currentPortion < 2) {
+                    elPrev.classList.add('hidden')
+                    // return
+                }
+                
+                elNext.addEventListener('click', async function () {
+                    currentPortion = currentPortion + 1
+                    
+                    const response = await fetch("http://localhost:5000/api/payment/usersList1", {
+                        method: "POST",
+                        headers: { "Accept": "application/json", "Content-Type": "application/json" },
+                        body: JSON.stringify({
+                            currentPortion
+                        })
+                    });
+                    if (response.ok === true) {
+                        const data = await response.json()
+                        console.log(data)
+                        let usersList = data.paginationData.users
+                        
+                        userList.innerHTML = ""
+
+                        for (i=0;i<=usersList.length-1;i++) {
+                            //create element
+                            var elem = document.createElement("li")
+            
+                            //create text for element
+                            var elemText = document.createTextNode(`
+                                ${usersList[i].role}: ${usersList[i].nicname} 
+                                з ${moment(usersList[i].registrDate).format('DD.MM.YY')}
+                                email: ${usersList[i].email} 
+                                на рахунку: ${usersList[i].balance.toFixed(2)} грн. 
+                                послугу активовано до: ${moment(usersList[i].endDay).format('DD.MM.YY')}
+                            `)
+            
+                            //adding text into element
+                            elem.appendChild(elemText)
+            
+                            //adding element into ul - .userList
+                            userList.appendChild(elem)
+                        }
+                        
+                        paginator.innerHTML = ''
+                        displayPaginationBtn()
+                        
+                    }
+
+                })
+
+                elPrev.addEventListener('click', async function () {
+                    currentPortion = currentPortion - 1
+                    
+                    
+                    console.log(`elPrev currentPortion: ${currentPortion}`)
+                    const response = await fetch("http://localhost:5000/api/payment/usersList1", {
+                        method: "POST",
+                        headers: { "Accept": "application/json", "Content-Type": "application/json" },
+                        body: JSON.stringify({
+                            currentPortion
+                        })
+                    });
+                    if (response.ok === true) {
+                        const data = await response.json()
+                        console.log(data)
+                        let usersList = data.paginationData.users
+                        
+                        userList.innerHTML = ""
+
+                        for (i=0;i<=usersList.length-1;i++) {
+                            //create element
+                            var elem = document.createElement("li")
+            
+                            //create text for element
+                            var elemText = document.createTextNode(`
+                                ${usersList[i].role}: ${usersList[i].nicname} 
+                                з ${moment(usersList[i].registrDate).format('DD.MM.YY')}
+                                email: ${usersList[i].email} 
+                                на рахунку: ${usersList[i].balance.toFixed(2)} грн. 
+                                послугу активовано до: ${moment(usersList[i].endDay).format('DD.MM.YY')}
+                            `)
+            
+                            //adding text into element
+                            elem.appendChild(elemText)
+            
+                            //adding element into ul - .userList
+                            userList.appendChild(elem)
+                        }
+                        
+                        paginator.innerHTML = ''
+                        elems = []
+                        start = currentPortion * portionSize - portionSize
+                        console.log(`elPrev elems: ${elems}`)
+                        displayPaginationBtn()
+
+                        //1 - 1
+                        //2 - 4
+                        //3 - 7
+                        //4 - 10
+                        //5 - 13
+                        
+                    }
+
+                })
+
             }
             displayPaginationBtn()
         }
@@ -87,7 +215,7 @@ window.addEventListener('load', function(){
 
 
     //display user-list after click on paginator-btns
-    async function displayUsersPage1 (rows) {
+    async function displayUsersPage1 () {
         // debugger
 
         //currentPage must be a number
@@ -103,7 +231,6 @@ window.addEventListener('load', function(){
             headers: { "Accept": "application/json", "Content-Type": "application/json" },
             body: JSON.stringify({
                 currentPage,
-                rows
             })
         });
         if (response.ok === true) {
--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -20,14 +20,27 @@ router.post('/users', cookieJwtAuth, async function(req, res){
 
 router.get('/usersList', cookieJwtAuth, async function(req, res){
     try {
+        const currentPortion = 1
         let currentPage = 0
-        let rows = 3
+        let rows = 5
         const allUsersData = await User.find()
 
         const pages = Math.ceil(allUsersData.length / rows)
 
-        const start = rows * currentPage
-        const end = start + rows;
+        //portion - count pagination button on 1 page
+        let portionSize = 2
+        //portions - count all pagination button
+        const portions = Math.ceil(pages / portionSize)
+
+        
+        let start = (currentPortion - 1) * portionSize
+        console.log(start)
+        // const end = currentPortion * portionSize
+
+        
+
+        // const start = rows * currentPage
+        const end = start + rows
         const usersData = allUsersData.slice(start, end)
 
         const users = []
@@ -46,7 +59,7 @@ router.get('/usersList', cookieJwtAuth, async function(req, res){
 
         
 
-        const paginationData = {users, currentPage, rows, pages}
+        const paginationData = {users, currentPage, rows, pages, currentPortion, portionSize, start, portions}
         return res.json({paginationData})
         // return res.render('service/users', {nicname1: 'vladim1', pagenumber3: 3})
     } catch (error) {
@@ -58,13 +71,13 @@ router.post('/usersList', cookieJwtAuth, async function(req, res, next){
     try {
         if(!req.body) return res.sendStatus(400)
         let currentPage = req.body.currentPage
-        let rows = 3
+        let rows = 5
         const allUsersData = await User.find()
 
         const pages = Math.ceil(allUsersData.length / rows)
 
         const start = rows * currentPage
-        const end = start + rows;
+        const end = start + rows
         const usersData = allUsersData.slice(start, end)
 
         const users = []
@@ -90,6 +103,60 @@ router.post('/usersList', cookieJwtAuth, async function(req, res, next){
             console.log(error)
             next(error)
     }
+            
+})
+
+router.post('/usersList1', cookieJwtAuth, async function(req, res, next){
+    try {
+        if(!req.body) return res.sendStatus(400)
+        let currentPortion = req.body.currentPortion
+        
+            let currentPage = 0
+            let rows = 5
+            const allUsersData = await User.find()
+    
+            const pages = Math.ceil(allUsersData.length / rows)
+            console.log(`pages: ${pages}`)
+    
+            //portion - count pagination button on 1 page
+            let portionSize = 2
+            //portions - count all pagination button
+            const portions = Math.ceil(pages / portionSize)
+    
+            
+            let start = (currentPortion - 1) * portionSize * rows
+            console.log(start)
+            // const end = currentPortion * portionSize
+            const end = start + rows
+    
+            
+    
+            // const start = rows * currentPage
+            // const end = start + rows;
+            const usersData = allUsersData.slice(start, end)
+    
+            const users = []
+            for (let i = 0; i < usersData.length; i++) {
+                let user = {
+                    nicname: usersData[i].nicname,
+                    registrDate: usersData[i].registrDate,
+                    email: usersData[i].email,
+                    role: usersData[i].status,
+                    balance: usersData[i].balance,
+                    endDay: usersData[i].endDay
+                }
+                users.push(user)
+            }
+            // console.log(users)
+    
+            
+    
+            const paginationData = {users, currentPage, rows, pages, currentPortion, portionSize, portions}
+            return res.json({paginationData})
+            // return res.render('service/users', {nicname1: 'vladim1', pagenumber3: 3})
+        } catch (error) {
+                console.log(error)
+        }
             
 })
 
